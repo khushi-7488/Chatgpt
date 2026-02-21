ccfea442b1e35db1f6f7ae7c68971fe10e939156
khushi-7488 <khushiitiwari88@gmail.com>
2026-02-21 21:27:04 +0530

    building frontend

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,16 +1,41 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useContext } from "react";
 
 function ChatWindow() {
+  const { prompt, setPrompt, reply, setReply, currThreadId, setCurrThreadId } =
+    useContext(MyContext);
+
+  const getReply = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: prompt,
+        threadId: currThreadId,
+      }),
+    };
+    try {
+      const response = await fetch("http://localhost:8080/api/chat", options);
+      await response.json;
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="chatWindow">
       <div className="navbar">
         <span>
-          ChatGPT&nbsp;<i class="fa-solid fa-chevron-down"></i>
+          ChatGPT&nbsp;<i className="fa-solid fa-chevron-down"></i>
         </span>
         <div className="userIconDiv">
-          <span>
-            <i class="fa-solid fa-user"></i>
+          <span className="userIcon">
+            <i className="fa-solid fa-user"></i>
           </span>
         </div>
       </div>
@@ -18,10 +43,15 @@ function ChatWindow() {
       <Chat></Chat>
 
       <div className="chatInput">
-        <div className="userInput">
-          <input type="text" placeholder="Ask anything" />
-          <div id="submit">
-            <i class="fa-solid fa-paper-plane"></i>
+        <div className="inputBox">
+          <input
+            type="text"
+            placeholder="Ask anything"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          ></input>
+          <div id="submit" onClick={getReply}>
+            <i className="fa-solid fa-paper-plane"></i>
           </div>
         </div>
         <p className="info">
